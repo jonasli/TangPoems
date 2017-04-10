1a8f10e295bce5ec771ae1cf5648c0d0f8c12eed
Jonas.Li <jonas.li@cdc.berlitz.net>
2017-04-10 17:59:11 +0800

    add the poem-annotation component

--- src/components/poem-annotation/poem-annotation.ts
+++ src/components/poem-annotation/poem-annotation.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 
 /*
-  Generated class for the PoemAnnotaion component.
+  Generated class for the PoemAnnotation component.
 
   See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
   for more info on Angular 2 Components.
 */
 @Component({
-  selector: 'poem-annotaion',
-  templateUrl: 'poem-annotaion.html'
+  selector: 'poem-annotation',
+  templateUrl: 'poem-annotation.html'
 })
-export class PoemAnnotaionComponent {
+export class PoemAnnotationComponent {
 
   text: string;
 
   constructor() {
-    console.log('Hello PoemAnnotaion Component');
+    console.log('Hello PoemAnnotation Component');
     this.text = 'Hello World';
   }
 

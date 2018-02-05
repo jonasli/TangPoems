70ae166086804d948332f019acd7bf057e5edc7f
jonas.li <jonas.li@cdc.berlitz.net>
2018-02-05 20:51:07 +0800

    pass android build

--- sign.sh
+++ sign.sh
@@ -1,1 +1,3 @@
+#https://www.jianshu.com/p/b39d7867fd60
+
 jarsigner -verbose -keystore tang300.keystore -signedjar /home/jonas/codes/Ionic/tang300/platforms/android/build/outputs/apk/android-release-signed.apk /home/jonas/codes/Ionic/tang300/platforms/android/build/outputs/apk/android-release-unsigned.apk   tang300.keystore
